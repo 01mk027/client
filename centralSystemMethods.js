e0defa31e6e354ff0517f248fec0dfd33f00244f
01mk027 <mustafagantep@gmail.com>
2022-09-16 16:16:05 +0300

    rr

--- centralSystemMethods.js
+++ centralSystemMethods.js
@@ -529,9 +529,9 @@ process.exit(0);
 });
 
 
-let cmsIpNPort = 'http://54.175.129.81:8000/';
+let cmsIpNPort = 'http://54.237.159.122:8000/';
 
-let ownIp = 'http://52.207.252.196:8001/';
+let ownIp = 'http://3.92.207.149:8001/';
 
 
 
